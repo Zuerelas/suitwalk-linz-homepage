dc9d35984e778f452f2047f2ef3806596cb9f930
Elias Pöschl <eliaspoe194@gmail.com>
2025-04-24 16:47:16 +0200

    fixed some things on headers and footers

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,11 +1,15 @@
 import './footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <footer>
             <div className="container">
                 <h3>Suitwalk Linz</h3>
-                <p>© {new Date().getFullYear()} - All Rights Reserved</p>
+                <p>Delta © {new Date().getFullYear()} - All Rights Reserved</p>
+            </div>
+            <div className='container'>
+                <h3><Link to="/impressum">Impressum</Link></h3>
             </div>
         </footer>
     )
