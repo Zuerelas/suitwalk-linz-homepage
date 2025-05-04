e64263d4cbcd5f591740ad5cf0dea3648306f72b
Elias Pöschl <eliaspoe194@gmail.com>
2025-05-04 21:10:19 +0200

    added some backend stuff

--- src/pages/anmeldung/suiter.jsx
+++ src/pages/anmeldung/suiter.jsx
@@ -31,7 +31,7 @@ function Suiter() {
                     <div className="telegram-widget-container">
                         <TelegramLoginWidget
                             botName="SuitwalkLinz_bot"
-                            authUrl="https://test.suitwalk-linz.at/api/telegram-auth"
+                            authUrl="http://localhost:3000/api/telegram-auth"
                             buttonSize="large"
                             requestAccess="write"
                             redirectUrl="https://test.suitwalk-linz.at/anmeldung/erfolgreich"
